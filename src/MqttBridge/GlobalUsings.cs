77f48573a4788963992d6efe2b5e0dc53ebc0966
SeppPenner <HansM2013@gmx.de>
2022-02-17 19:19:40 +0100

    Smaller changes.

--- src/MqttBridge/GlobalUsings.cs
+++ src/MqttBridge/GlobalUsings.cs
@@ -1,4 +1,4 @@
-#pragma warning disable IDE0065 // Die global using-Anweisung wurde falsch platziert.
+#pragma warning disable IDE0065 // Die using-Anweisung wurde falsch platziert.
 global using System.Reflection;
 global using System.Text;
 
@@ -9,4 +9,4 @@ global using MQTTnet.Server;
 global using Newtonsoft.Json;
 
 global using Serilog;
-#pragma warning restore IDE0065 // Die global using-Anweisung wurde falsch platziert.
+#pragma warning restore IDE0065 // Die using-Anweisung wurde falsch platziert.
